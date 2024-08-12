c5a40e1123d351080d64cfaa52336928133c5d3d
Tyre <122072344+tyrr72@users.noreply.github.com>
2024-08-12 19:16:17 +1000

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+using YizziCamModV2;
+
+
+
+namespace YizFix
+{
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        void Start()
+        {
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            CameraController.Instance.fpv = true;
+            CameraController.Instance.smoothing = 0.8f;
+            CameraController.Instance.FirstPersonCamera.fieldOfView = 110;
+            CameraController.Instance.ThirdPersonCamera.fieldOfView = 110;
+            CameraController.Instance.FirstPersonCamera.nearClipPlane = 0.01f;
+            CameraController.Instance.ThirdPersonCamera.nearClipPlane = 0.01f;
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace YizFix
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.tyrr.gorillatag.yizfix";
+        public const string Name = "YizFix";
+        public const string Version = "1.0.0";
+    }
+}
